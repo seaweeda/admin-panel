951f5d438b85ed08ce1b5d790f3f76394c7abf51
seaweeda <widawibowo3@gmail.com>
2024-12-23 22:59:32 +0700

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "@/components/admin-panel/AuthProvider";
+import App from "./App";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthProvider>
+          <App>{children}</App>
+        </AuthProvider>
+
+        <Toaster position="bottom-center" reverseOrder={false} />
       </body>
     </html>
   );
